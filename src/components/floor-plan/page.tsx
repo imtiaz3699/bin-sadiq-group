768013780e9ce06aa43590206c1dae2b3c5035c4
imtiaz3699 <imtiazali78652@gmail.com>
2023-09-14 17:00:29 +0500

    last

--- src/components/floor-plan/page.tsx
+++ src/components/floor-plan/page.tsx
@@ -4,7 +4,7 @@ import SecondNavbar from "@/components/SecondNavbar/SecondNavbar";
 export default function FloorPlan() {
     return <div>
     
-        <div className="w-full h-[100vh] bg-no-repeat relative fasdfasdfhasdlfjahsdlfjashfdlakjsd fafsdfasdfasdfsadfasd bg-cover" style = {{backgroundImage:'url()'}}>
+        <div className="w-full h-[100vh] bg-no-repeat relative bg-cover" style = {{backgroundImage:'url()'}}>
             <SecondNavbar/>
         </div>
     </div>

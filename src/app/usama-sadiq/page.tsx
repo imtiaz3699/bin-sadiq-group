70393f16b147b2817f6dea75182708090e77bb0c
imtiaz3699 <imtiazali78652@gmail.com>
2023-09-23 18:37:38 +0500

    last

--- src/app/usama-sadiq/page.tsx
+++ src/app/usama-sadiq/page.tsx
@@ -10,17 +10,17 @@ export default function UsamaSadiq(){
     <MyNavbar/>
 
     </div>
-    <div className="w-full py-10 bg-lead-color flex flex-col items-center justify-center">
-<h1 className="text-[30px]">MUHAMMAD USAMA SADIQ</h1>
+    <div className=" px-20 w-full py-10 bg-lead-color flex flex-col items-center justify-center">
+<h1 className="text-golden font-bold text-center text-[30px] sm:text-[40px] md:text-[50px] font-bold leading-[56px] text-white text-center">MUHAMMAD USAMA SADIQ</h1>
+<div className="text-white text-[20px] text-center mt-5">Usama Sadiq is a property maven with an experience arching over ten years in construction and
+development. As the founder and CEO of Bin Sadiq Group, he has adeptly administered team
+management, financial forecasting, investment management and people development and
+training. Accrediting his holistic and competent lens, he has persisted an inevitable impact in the
+realtors expanding business ever since embarking into property development conglomerate</div>
     </div>
-    <div className="flex py-20 flex-col items-center">
-        <h1>Follow Usama Sadiq On Social Media Platforms</h1>
-        <div className="flex flex-row items-center justify-center">
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-        </div>
+    <div>
     </div>
+    
 
     </>
 }
